e5e10098470f3432111cf4db3f7d0682c4e5f65e
rdixon555 <ryan555@sky.com>
2020-05-30 16:18:21 +0100

    carousel styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import { GlobalBackground } from '../style/GlobalBackground'
 import Img from 'gatsby-image'
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Carousel from 'react-bootstrap/Carousel'
+import { Container, Row, Col } from 'react-bootstrap'
 import MyNavBar from '../components/Navbar'
 
-
 const Home = ({ data: { home } }) => (
-<div>
-  <MyNavBar />
-  
+  <div>
+    <MyNavBar />
 
-  <div className="sheet__inner">
-    <h1 className="sheet__title">{home.headline}</h1>
-    <h1 className="sheet__title">{home.contactEmail}</h1>
+    <GlobalBackground>
+    
+      <div className="sheet__inner">
+        <h1 className="sheet__title">{home.headline}</h1>
+        <h1 className="sheet__title">{home.contactEmail}</h1>
+      </div>
+     
+    </GlobalBackground>
+
+    <Container>
+      <Row>
+        <Col>
+          <div className = "carousel-styling">
+              <Carousel slide={true} controls={true} indicators={true} interval={1500}>
+                  {home.gallery.map(({ fluid }) => (
+                    <Carousel.Item>
+                      <img  className="d-block w-100" alt={fluid.title} key={fluid.src} src={fluid.src} />
+                    </Carousel.Item>
+                  ))}
+                </Carousel>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   </div>
-
-  <Carousel slide={true} controls={true} indicators={true} interval={1500}>
-    {home.gallery.map(({ fluid }) => (
-      <Carousel.Item>
-        <img className="carousel-styling" alt={fluid.title} key={fluid.src} src={fluid.src} />
-      </Carousel.Item>
-    ))}
-  </Carousel>
-  
-</div>
 )
 
 export default Home
---
+++ src/style/GlobalBackground.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const GlobalBackground = styled.div`
+    position: relative;
+    width: 100%;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
